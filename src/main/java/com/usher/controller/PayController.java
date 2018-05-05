edf91e64a37b259ba3faa52ab757663460b78fae
legolas007 <oukuanou@163.com>
2018-05-05 23:53:36 +0800

    支付测试

--- src/main/java/com/usher/controller/PayController.java
+++ src/main/java/com/usher/controller/PayController.java
@@ -1,10 +1,17 @@
 package com.usher.controller;
 
+import com.lly835.bestpay.model.PayResponse;
+import com.usher.dto.OrderDTO;
+import com.usher.enums.ResultEnum;
+import com.usher.exception.SellException;
 import com.usher.service.OrderService;
+import com.usher.service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * @Author: Usher
@@ -18,6 +25,38 @@ public class PayController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private PayService payService;
 
+    @GetMapping("/create")
+    public ModelAndView create(@RequestParam("orderId") String orderId,
+                               @RequestParam("returnUrl") String returnUrl,
+                               Map<String, Object> map) {
+        //1. 查询订单
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        if (orderDTO == null) {
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
 
+        //2. 发起支付
+        PayResponse payResponse = payService.create(orderDTO);
+
+        map.put("payResponse", payResponse);
+        map.put("returnUrl", returnUrl);
+
+        return new ModelAndView("pay/create", map);
+    }
+
+    /**
+     * 微信异步通知
+     *
+     * @param notifyData
+     */
+    @PostMapping("/notify")
+    public ModelAndView notify(@RequestBody String notifyData) {
+        payService.notify(notifyData);
+
+        //返回给微信处理结果
+        return new ModelAndView("pay/success");
+    }
 }
