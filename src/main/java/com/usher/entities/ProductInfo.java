076213b5a4181ab20dfb8b394e5045b56cf0f301
legolas007 <oukuanou@163.com>
2018-05-05 12:43:06 +0800

    商品信对接息后台

--- src/main/java/com/usher/entities/ProductInfo.java
+++ src/main/java/com/usher/entities/ProductInfo.java
@@ -1,10 +1,15 @@
 package com.usher.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.usher.enums.ProductStatusEnum;
+import com.usher.utils.EnumUtil;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @Author: Usher
@@ -13,6 +18,7 @@ import java.math.BigDecimal;
  */
 @Entity
 @Data
+@DynamicUpdate
 public class ProductInfo {
     @Id
     private String productId;
@@ -27,8 +33,19 @@ public class ProductInfo {
 
     private String productIcon;
 
-    private Integer productStatus;
+    //状态，0正常，1下架
+    private Integer productStatus = ProductStatusEnum.UP.getCode();
 
+    //类目编号
     private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    @JsonIgnore
+    public ProductStatusEnum getProductStatusEnum(){
+        return EnumUtil.getByCode(productStatus,ProductStatusEnum.class);
+    }
 
 }
