adca9b7c1837fbdf66da9e0cdb1536ef8bee11e3
legolas007 <oukuanou@163.com>
2018-05-05 23:53:07 +0800

    相关变量分离

---
+++ src/main/java/com/usher/constant/CookieConstant.java
@@ -0,0 +1,13 @@
+package com.usher.constant;
+
+/**
+ * @Author: Usher
+ * @Description:
+ * cookie常量
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/usher/constant/RedisConstant.java
@@ -0,0 +1,13 @@
+package com.usher.constant;
+
+/**
+ * @Author: Usher
+ * @Description:
+ * redis常量
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    Integer EXPIRE = 7200; //2小时
+}
