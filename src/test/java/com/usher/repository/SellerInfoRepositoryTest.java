f46e301f6b6637fa84fd549528b0c527f049e1dd
legolas007 <oukuanou@163.com>
2018-05-05 23:51:25 +0800

    test

---
+++ src/test/java/com/usher/repository/SellerInfoRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.usher.repository;
+
+import com.usher.entities.SellerInfo;
+import com.usher.utils.KeyUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: Usher
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class SellerInfoRepositoryTest {
+
+    @Autowired
+    private SellerInfoRepository repository;
+
+    @Test
+    public void save(){
+        SellerInfo sellerInfo = new SellerInfo();
+        sellerInfo.setId(KeyUtil.genUniqueKey());
+        sellerInfo.setUsername("admin");
+        sellerInfo.setPassword("admin");
+        sellerInfo.setOpenid("abc");
+
+        SellerInfo result = repository.save(sellerInfo);
+        Assert.assertNotNull(result);
+    }
+    @Test
+    public void findByOpenid() {
+        SellerInfo result = repository.findByOpenid("abc");
+        Assert.assertEquals("abc", result.getOpenid());
+
+    }
+}
