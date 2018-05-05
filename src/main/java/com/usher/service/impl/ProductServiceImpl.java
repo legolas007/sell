528faa17284873544f25b9693615cec153c1c6b6
legolas007 <oukuanou@163.com>
2018-05-05 12:44:09 +0800

    后台商品上下架Service

--- src/main/java/com/usher/service/impl/ProductServiceImpl.java
+++ src/main/java/com/usher/service/impl/ProductServiceImpl.java
@@ -75,4 +75,40 @@ public class ProductServiceImpl implements ProductService {
             repository.save(productInfo);
         }
     }
+
+    /**
+     * 上架
+     * @param productId
+     * @return
+     */
+    @Override
+    public ProductInfo onSale(String productId) {
+        ProductInfo productInfo = repository.findOne(productId);
+        if (productInfo == null)
+            throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+        if (productInfo.getProductStatusEnum() == ProductStatusEnum.UP)
+            throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
+
+        productInfo.setProductStatus(ProductStatusEnum.UP.getCode());
+
+        return repository.save(productInfo);
+    }
+
+    /**
+     * 下架
+     * @param productId
+     * @return
+     */
+    @Override
+    public ProductInfo offSale(String productId) {
+        ProductInfo productInfo = repository.findOne(productId);
+        if (productInfo == null)
+            throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+        if (productInfo.getProductStatusEnum() == ProductStatusEnum.DOWN)
+            throw new SellException(ResultEnum.PRODUCT_STATUS_ERROR);
+
+        productInfo.setProductStatus(ProductStatusEnum.DOWN.getCode());
+
+        return repository.save(productInfo);
+    }
 }
