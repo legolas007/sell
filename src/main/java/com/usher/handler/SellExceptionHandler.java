d50a8e9fb3b10973da0497b9d67487ee12f584a7
legolas007 <oukuanou@163.com>
2018-05-05 23:54:22 +0800

    异常处理

---
+++ src/main/java/com/usher/handler/SellExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.usher.handler;
+
+import com.usher.config.ProjectUrlConfig;
+import com.usher.exception.SellerAuthorizeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Author: Usher
+ * @Description:
+ * 全局AOP登录验证异常处理
+ */
+@ControllerAdvice
+public class SellExceptionHandler {
+
+    @Autowired
+    private ProjectUrlConfig projectUrlConfig;
+
+    //拦截登录异常
+    //http://sell.natapp4.cc/sell/wechat/qrAuthorize?returnUrl=http://sell.natapp4.cc/sell/seller/login
+    @ExceptionHandler(value = SellerAuthorizeException.class)
+    public ModelAndView handlerAuthorizeException(){
+        return new ModelAndView("redirect:"
+                .concat(projectUrlConfig.getWechatOpenAuthorize())
+                .concat("/sell/wechat/qrAuthorize")
+                .concat("?returnUrl=")
+                .concat(projectUrlConfig.getSell())
+                .concat("/sell/seller/login"));
+    }
+}
