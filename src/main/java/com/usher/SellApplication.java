2edd11430ce9b9d5d9ddd3f3a8309c8016fb6302
legolas007 <oukuanou@163.com>
2018-05-06 22:05:42 +0800

    缓存

--- src/main/java/com/usher/SellApplication.java
+++ src/main/java/com/usher/SellApplication.java
@@ -1,9 +1,13 @@
 package com.usher;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@MapperScan(value = "com.usher.entities.mapper")
+@EnableCaching
 public class SellApplication {
 
 	public static void main(String[] args) {
