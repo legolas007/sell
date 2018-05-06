ad4535e7ae1e08d8b02619d6c3dcec7da5d5542d
legolas007 <oukuanou@163.com>
2018-05-06 22:03:43 +0800

    mapper

---
+++ src/main/java/com/usher/entities/mapper/ProductCategoryMapper.java
@@ -0,0 +1,56 @@
+package com.usher.entities.mapper;
+
+import com.usher.entities.ProductCategory;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Usher
+ * @Description:
+ */
+@Mapper
+public interface ProductCategoryMapper {
+
+    //xml
+    List<ProductCategory> selectByCategoryType(Integer categoryType);
+
+    ProductCategory selectByCategoryName(String categoryName);
+
+    @Insert("insert into product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{category_type, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+
+    @Insert("insert into product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{categoryType, jdbcType=INTEGER})")
+    int insertByObject(ProductCategory productCategory);
+
+    @Select("select * from product_category where category_type=#{categoryType} ")
+    //数据库与实体类对应
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    ProductCategory findByCategoryType(Integer categoryType);
+
+
+    @Select("select * from product_category where category_name = #{categoryName}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    List<ProductCategory> findByCategoryName(String categoryName);
+
+    @Update("update product_category set category_name=#{categoryName} where category_type=#{categoryType}")
+    int updateByCategoryType(@Param("categoryName") String categoryName,
+                             @Param("categoryType") Integer categoryType);
+
+    @Update("update product_category set category_name = #{categoryName} where category_type = #{categoryType}")
+    int updateByObject(ProductCategory productCategory);
+
+    @Delete("delete from product_category where category_type = #{categoryType}")
+    int deleteByCategoryType(Integer categoryType);
+
+}
